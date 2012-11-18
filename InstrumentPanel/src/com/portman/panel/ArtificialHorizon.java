6e8218e53eda6f7a8782abbafe94c71d5ca9df78
leonpo <leon.portman@gmail.com>
2012-11-18 15:25:14 +0200

    Fix references

--- InstrumentPanel/src/com/portman/panel/ArtificialHorizon.java
+++ InstrumentPanel/src/com/portman/panel/ArtificialHorizon.java
@@ -6,7 +6,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
-import android.graphics.Typeface;
 
 import android.os.Bundle;
 import android.os.Parcelable;
