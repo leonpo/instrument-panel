86d9652965a2c83f2f453553b2c14fc4dde68167
leonpo <leon.portman@gmail.com>
2012-12-05 17:46:44 +0200

    Fix fuel tank gauges

--- InstrumentPanel/lua/telemetry.lua
+++ InstrumentPanel/lua/telemetry.lua
@@ -43,10 +43,40 @@ AfterNextFrame=function(self)
 	local Oil_Temperature = MainPanel:get_argument_value(30) * 100
 	local Oil_Pressure = MainPanel:get_argument_value(31) * 200
 	local Fuel_Pressure = MainPanel:get_argument_value(32) * 25
-	local Fuel_Tank_Left = MainPanel:get_argument_value(155) * 92
-	local Fuel_Tank_Right = MainPanel:get_argument_value(156) * 92
-	local Fuel_Tank_Fuselage = MainPanel:get_argument_value(160) * 85
 	
+	local Fuel_Tank_Left = MainPanel:get_argument_value(155)
+	local Fuel_Tank_Right = MainPanel:get_argument_value(156)
+	local Fuel_Tank_Fuselage = MainPanel:get_argument_value(160)
+	
+	if	Fuel_Tank_Left <= 0.2 then Fuel_Tank_Left = Fuel_Tank_Left * 5
+    elseif Fuel_Tank_Left <= 0.36 then Fuel_Tank_Left = Fuel_Tank_Left * 15
+    elseif Fuel_Tank_Left <= 0.52 then Fuel_Tank_Left = Fuel_Tank_Left * 30
+    elseif Fuel_Tank_Left <= 0.65 then Fuel_Tank_Left = Fuel_Tank_Left * 45
+    elseif Fuel_Tank_Left <= 0.77 then Fuel_Tank_Left = Fuel_Tank_Left * 60
+    elseif Fuel_Tank_Left <= 0.92 then Fuel_Tank_Left = Fuel_Tank_Left * 75
+    else Fuel_Tank_Left = Fuel_Tank_Left * 92
+    end
+    
+	if	Fuel_Tank_Right <= 0.2 then Fuel_Tank_Right = Fuel_Tank_Right * 5
+    elseif Fuel_Tank_Right <= 0.36 then Fuel_Tank_Right = Fuel_Tank_Right * 15
+    elseif Fuel_Tank_Right <= 0.52 then Fuel_Tank_Right = Fuel_Tank_Right * 30
+    elseif Fuel_Tank_Right <= 0.65 then Fuel_Tank_Right = Fuel_Tank_Right * 45
+    elseif Fuel_Tank_Right <= 0.77 then Fuel_Tank_Right = Fuel_Tank_Right * 60
+    elseif Fuel_Tank_Right <= 0.92 then Fuel_Tank_Right = Fuel_Tank_Right * 75
+    else Fuel_Tank_Right = Fuel_Tank_Right * 92
+    end
+    
+    if	Fuel_Tank_Fuselage <= 0.12 then Fuel_Tank_Fuselage = Fuel_Tank_Fuselage * 10
+    elseif Fuel_Tank_Fuselage <= 0.28 then Fuel_Tank_Fuselage = Fuel_Tank_Fuselage * 20
+    elseif Fuel_Tank_Fuselage <= 0.40 then Fuel_Tank_Fuselage = Fuel_Tank_Fuselage * 30
+    elseif Fuel_Tank_Fuselage <= 0.51 then Fuel_Tank_Fuselage = Fuel_Tank_Fuselage * 40
+    elseif Fuel_Tank_Fuselage <= 0.62 then Fuel_Tank_Fuselage = Fuel_Tank_Fuselage * 50
+    elseif Fuel_Tank_Fuselage <= 0.72 then Fuel_Tank_Fuselage = Fuel_Tank_Fuselage * 60
+    elseif Fuel_Tank_Fuselage <= 0.83 then Fuel_Tank_Fuselage = Fuel_Tank_Fuselage * 70
+    elseif Fuel_Tank_Fuselage <= 0.96 then Fuel_Tank_Fuselage = Fuel_Tank_Fuselage * 80
+    else Fuel_Tank_Fuselage = Fuel_Tank_Fuselage * 85
+    end    
+		
 	my_send = socket.protect(function()
 		if c2 then
 			socket.try(c2:send(string.format("{ 'AirspeedNeedle':%.2f, 'Altimeter_10000_footPtr':%.2f, 'Altimeter_1000_footPtr':%.2f, 'Altimeter_100_footPtr':%.2f, 'Variometer':%.2f, 'TurnNeedle':%.2f, 'Slipball':%.2f, 'CompassHeading':%.2f, 'Landing_Gear_Handle':%.2f, 'Manifold_Pressure':%.2f, 'Engine_RPM':%.2f, 'AHorizon_Pitch':%.2f, 'AHorizon_Bank':%.2f, 'AHorizon_PitchShift':%.2f, 'GyroHeading':%.2f, 'Oil_Temperature':%.2f, 'Oil_Pressure':%.2f, 'Fuel_Pressure':%.2f, 'Fuel_Tank_Left':%.2f, 'Fuel_Tank_Right':%.2f, 'Fuel_Tank_Fuselage':%.2f }\n", AirspeedNeedle, Altimeter_10000_footPtr, Altimeter_1000_footPtr, Altimeter_100_footPtr, Variometer, TurnNeedle, Slipball, CompassHeading, Landing_Gear_Handle, Manifold_Pressure, Engine_RPM, AHorizon_Pitch, AHorizon_Bank, AHorizon_PitchShift, GyroHeading, Oil_Temperature, Oil_Pressure, Fuel_Pressure, Fuel_Tank_Left, Fuel_Tank_Right, Fuel_Tank_Fuselage)))
